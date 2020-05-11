429d0aa29aa2c8207b429674616c11c3cc71b64c
Santiago Alessandri <santiago@salessandri.name>
2020-05-10 22:52:59 -0700

    Update package metadata
    - Change email to one for the project
    - Generate the long description from the README.md

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="backup-to-dropbox",
     version="0.2.0",
@@ -14,8 +17,10 @@ setup(
     ],
 
     author="Santiago Alessandri",
-    author_email="santiago@salessandri.name",
+    author_email="backup-to-dropbox@salessandri.name",
     description="Tool to generate backups (tar.gz files) and upload them to Dropbox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     keywords="backup dropbox",
     url="https://github.com/salessandri/backup-to-dropbox",
     classifiers=[
